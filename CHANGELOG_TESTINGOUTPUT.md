0d65cf937b9ebef48b8a00c59f82756d1eac1d85
Jason Jacobson <jdesigns.jason@gmail.com>
2022-05-04 09:31:42 -0500

    feat(generator): Add output generator testing

---
+++ CHANGELOG_TESTINGOUTPUT.md
@@ -0,0 +1,66 @@
+# Changelog
+
+All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
+
+### [1.3.1](///compare/v1.3.0...v1.3.1) (2022-04-05)
+
+### Features
+
+* **washerApi:** Added new routes 7a75802
+* **washerClient:**  WDF-6 Added new components e2c8ffc
+
+### Bug Fixes
+
+* **element:** Added a new element f25c801
+* **list:** Added missing list items 3f05b74
+
+## [1.3.0](///compare/v1.2.2...v1.3.0) (2022-04-05)
+
+
+### Features
+
+* **washerApi:** Added new routes 7a75802
+* **washerClient:** Added new components e2c8ffc
+
+### [1.2.2](///compare/v1.2.1...v1.2.2) (2022-04-05)
+
+### [1.2.1](///compare/v1.2.0...v1.2.1) (2022-04-05)
+
+
+### Bug Fixes
+
+* **element:** Added a new element f25c801
+
+## [1.2.0](///compare/v1.1.1...v1.2.0) (2022-03-31)
+
+
+### Features
+
+* **list:** Added the list items 7b24823
+
+
+### Bug Fixes
+
+* **list:** Added missing list items 3f05b74
+
+### [1.1.1](///compare/v1.1.0...v1.1.1) (2022-03-31)
+
+
+### Bug Fixes
+
+* **text:** Text copy change 5399926
+* **text:** Updated the text d01a3ae
+
+## [1.1.0](///compare/v1.0.0...v1.1.0) (2022-03-31)
+
+
+### Features
+
+* **index:** Updated the index file df41d57
+
+## 1.0.0 (2022-03-31)
+
+
+### Features
+
+* **initial:** First commmit a06002c
---
+++ generator.js
@@ -0,0 +1,97 @@
+'use strict';
+const fs = require('fs');
+const baseLink = 'https://bofcmportal.atlassian.net/browse/'
+let version = ''
+
+console.warn('process argv', process.argv)
+
+function fileData() {
+	fs.readFile(`${__dirname}/CHANGELOG_TESTINGOUTPUT.md`, 'utf8', (err, data) => {
+		if (err) {
+			console.error('err', err)
+			return;
+		}
+
+		// console.warn('data', data);
+		if (data) {
+			const issues = getCommitsFromVersion(data)
+			const issuesWLinks = createLinks(issues)
+			createOutputFile(issuesWLinks)
+		} else {
+			console.error('No data from file')
+		}
+	})
+}
+
+function getCommitsFromVersion(data) {
+	const versionRegex = /\d.\d.\d/g
+	const versions = data.match(versionRegex)
+	// RegExp for \s\S needs additional slash as being stripped
+	version = versions[0]
+	const allCommitsRegex = RegExp(versions[0] + '([\\s\\S]*?)\]', 'gm')
+	const allCommits = data.match(allCommitsRegex)
+	const commitRegex = /\*\*([\s\S]*?)\n/g
+	const issues = []
+
+	for (let i = 0; i < allCommits.length; i++) {
+		const match = allCommits[i].match(commitRegex)
+
+		if (match && match.length > 0) {
+			for (let m = 0; m < match.length; m++) {
+				issues.push(match[m])
+			}
+		}
+	}
+
+	return issues;
+}
+
+function createLinks(issues) {
+	const output = []
+	const WDFRegex = /([a-zA-Z]+(-[0-9]+)+)/g
+
+	for (let i = 0; i < issues.length; i++) {
+		const issue = {
+			title: issues[i]
+		}
+
+		if (WDFRegex.test(issues[i])) {
+			const match = issues[i].match(WDFRegex)
+			issue['link'] = `[[${match[0]}]](${baseLink}${match[0]})`
+		}
+
+		output.push(issue)
+	}
+
+	return output
+}
+
+function createOutputFile(data) {
+	let outski = `${version}\n${data.map(d => {
+		if (d.link) {
+			return d.title + d.link + '\n\n'
+		}
+		return d.title + '\n'
+	})}`;
+	outski = outski.replace(/,/g, '')
+	outski = outski.replace(/\*\*/g, '')
+	console.warn('outski', outski);
+
+	fs.writeFile(`${__dirname}/outski.txt`, outski, err => {
+		if (err) console.error('failed to write')
+	})
+}
+
+// worker
+// #{2}[^\n]*\n([\s\S]*?)#{2}
+
+// capture first version #
+// \d.\d.\d
+// then capture starting with that
+// 1.3.0([\s\S]*?)\]
+// from there capture lines and if they match a pattern such as 'WDF-' append to provided url
+// \*\*([\s\S]*?)\n
+
+// NOTE: need to test a multi line desciption commit
+
+fileData()
